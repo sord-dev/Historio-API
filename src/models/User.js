e75de533ec9921395df3a4888c78e09fbbfb7b65
Julia Barbosa <juliadealencarb@gmail.com>
2023-03-06 22:01:33 +0000

    Adding sign up feature and creating a new class to model user.

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcrypt");
+
+
+// Creating a class to model user. This class has a method to encrypt the user password. 
+module.exports = class User {
+
+    constructor (username, password, statsId) {
+       this.id = undefined;
+       this.username = username;
+       this.password = password;
+       this.statsId = statsId;
+    }
+   
+    async encryptPassword() {
+        const salt = await bcrypt.genSalt()
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+
+    setId (id) {
+        this.id = id;
+    }
+}
