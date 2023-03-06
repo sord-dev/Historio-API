4f3406ddae040e934097ddb0b92d90c91865532c
Julia Barbosa <juliadealencarb@gmail.com>
2023-03-06 11:02:50 +0000

    Creating SQLite database and creating endpoint to display all users (not final).

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const quizRouter = require('./routes/quizRouter');
+const userLogIn = require('./routes/auth.js');
 const app = express();
 
+
 const port = 3000;
+
+
 
 app.use(cors());
 
 app.use('/quiz', quizRouter);
+app.use('/login', userLogIn)
 
 app.listen(port, () => console.log(`listening on port - ${port}`))
---
+++ src/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const userLogIn = express.Router();
+
+const userDB = require("../helpers/userDB.js");
+md5 = require("md5");
+const bodyParser = require("body-parser");
+
+userLogIn.use(bodyParser.urlencoded({extended: false}));
+userLogIn.use(bodyParser.json());
+
+userLogIn.get('/users', (req, res) => {
+    const sql = "SELECT * FROM users"
+    const params = []
+    userDB.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message": "sucess",
+            "data": rows
+        })
+    })
+})
+
+module.exports = userLogIn
