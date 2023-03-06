a41b84e53ae2ca9301b080dd463cb0780e1c0076
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-06 10:26:29 +0000

    add quizRouter basic routes

--- src/routes/quizRouter.js
+++ src/routes/quizRouter.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const quizRouter = express.Router();
 
+// get and calculate leaderboard
 quizRouter.get('/leaderboard', (req, res) => {
     res.json([{name: 'bob'}, {name: 'dean'}])
+})
+
+// get and questions by type
+quizRouter.get('/questions/:type', (req, res) => {
+    // depending on type, return associated questions
+    res.json([{question1: 'bob'}, {question2: 'dean'}])
 })
 
 module.exports = quizRouter;
