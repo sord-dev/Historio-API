1c103b7e6de2018355289ee69b66ac28fb3c7668
Julia Barbosa <juliadealencarb@gmail.com>
2023-03-07 21:31:04 +0000

    Adding helper function to UserServices.

--- src/helpers/UserServices.js
+++ src/helpers/UserServices.js
@@ -10,8 +10,17 @@ function getUser(username) {
     return users.find(user => user.username == username);
 }
 
-// function addUser(user) {
-//     arrUsers.push(user);
+function updateUserStatsID(username, newStatsID) {
+    let targetUser = getUser(username);
+    const targetUserIndex = users.indexOf(targetUser);
+    users[targetUserIndex].statsID = newStatsID;
+}
+
+// If we use PUT request to update user...
+// function updateUser(user) {
+//     const targetUserIndex = users.indexOf(x => x.username == user.username);
+//     if (targetUserIndex > -1)
+//         users[targetUserIndex] = user;
 // }
 
 // // generate maxId...
