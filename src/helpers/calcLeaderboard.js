751fdc5730eefbbedbf41046675f0e74c1017f0a
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-07 22:17:21 +0000

    adding logging middleware and updating helper functions

--- src/helpers/calcLeaderboard.js
+++ src/helpers/calcLeaderboard.js
@@ -1,9 +1,4 @@
-// gets all the users by stats, sorts them and returns top 3
-// function calculate(formattedUsers, length = 3) {
-//     return formattedUsers.sort((a, z) => z.stats.level - a.stats.level).slice(0, length);
-// }
-
-// Same function updated to use totalXP
+// Function updated to use totalXP
 // gets all the users by stats, sorts them and returns top 5
 function calculate(formattedUsers, length = 5) {
     return formattedUsers.sort((a, z) => z.stats.totalXP - a.stats.totalXP).slice(0, length);
---
+++ src/helpers/logRequests.js
@@ -0,0 +1,9 @@
+function logRequests(req, res, next) {
+    const {url, method} = req;
+
+    console.log(`${method} ${url}`);
+
+    next()
+}
+
+module.exports = logRequests;
