862f95d7ece3eaa5a3657ee895a790e91d572ea0
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-08 12:59:20 +0000

    updateXP + PATCH /me tweaks

--- src/helpers/updateXP.js
+++ src/helpers/updateXP.js
@@ -1,13 +1,14 @@
-let stats = require('../config/stats.json')
+let stats = require("../config/stats.json");
 
 // Function takes in a statsID as parameter, and increases that IDs score by 5 plus a random bit (just to make it SEEEM more exciting)
-function updateXP(statsID) {
-    for (stat of stats) {
-        if (stat.statsID === statsID) {
-            stat.totalXP += (10 + Math.floor((Math.random() * 5)))
-            return stat;
-        }
+function updateXP(username, statsID) {
+  for (let stat of stats) {
+    if (stat.statsID == statsID) {
+      stat.totalXP += 10;
+
+      return { message: `increase ${username || statsID} stats by ${10}` };
     }
+  }
 }
 
 module.exports = updateXP;
