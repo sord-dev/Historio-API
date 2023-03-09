c836cf93c127187f11876950aee5adf8c11ca004
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-09 11:30:54 +0000

    updateXP function

--- src/helpers/updateXP.js
+++ src/helpers/updateXP.js
@@ -5,19 +5,12 @@ function updateXP(username, statsID) {
   for (let stat of stats) {
     if (stat.statsID == statsID) {
       let increase = 20;
-      // calculate prev level and new level
-      let prev = Math.floor(stats.totalXP / 100)
+
       stat.totalXP += increase;
-      let level = Math.floor(stats.totalXP / 100);
 
-      // if they leveled up then return level up message else return stat increase
-      if(prev < level) {
-        return { message: `${username} LEVEL UP TO ${level}` };
-      } else {
-
-        return { message: `increase ${username || statsID} stats by ${increase}` };
-      }
-
+      return {
+        message: `increase ${username || statsID} stats by ${increase}`,
+      };
     }
   }
 }
